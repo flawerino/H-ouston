7432487c75d2c0bbabd89c478dfec168f0dc2898
Chaya Mazzariol <chaya@Air-di-Chaya.homenet.telecomitalia.it>
2023-12-16 16:08:54 +0100

    PEP8 check

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,13 +13,15 @@ import pandas as pd
 
 app = FastAPI()
 
-df = pd.read_csv('/app/app/data.csv', sep=';')
+df = pd.read_csv('/app/app/data.csv', sep = ';')
 # Converting all the numbers inside the dataframe as strings for JSON
 df = df.astype(str)
 
-# Extract from CSV file the names of the districts and add them in a string
+# Creating a string with districts' name from CSV file
+
+
 def print_province_names():
-    print('Welcome to "Find the nearest theater around you". We know the theaters in these districts:')
+    print("Find the theaters in these districts:")
     # Checking the existence of "District" column
     if 'Provincia' not in df.columns:
         return "The dataframe does not contain any 'District' column"
@@ -34,11 +36,15 @@ print(result)
 
 
 @app.get('/csv_show')
+
+
 def read_and_return_csv():
     aux = df['CAP'].values
     return{"CAP": str(aux.argmin())}
 
 @app.get('/')
+
+
 def read_root():
     """
     Root endpoint for the backend.
@@ -49,6 +55,8 @@ def read_root():
     return {"Hello": "Veneto"}
 
 @app.get('/query/{district_name}')
+
+
 def read_item(district_name: str):
     """
     Endpoint to query information based on district_name.
@@ -57,26 +65,43 @@ def read_item(district_name: str):
     Returns:
         dict: Information for the provided district_name.
     """
-   # Step 2: Convert district_name to title case for consistency
+    # Convert district_name to title case for consistency
     district_name = district_name.title()
     #small = df[df['Provincia'] == district_name][['Nome']]
-    # Step 3: Check if the district_name exists in the 'name' column of the DataFrame
+    # Check if the district_name exists in the 'name' column of the DataFrame
     if district_name in df['Provincia'].values:
-        # Step 4: Get all rows where 'Provincia' is equal to district_name
+    # Get rows where 'Provincia' is equal to district_name
+        district_info = df[df['Provincia'] == district_name].to_dict(orient='records')
+        
+        for info in district_info:
+            print(info)
+        
+        return {"district_name": district_name, "district_info": district_info}
+    else:
+        return {"Error": "District not found"}
+    
+    """
+    if district_name in df['Provincia'].values:
+        # Get rows where 'Provincia' is equal to district_name
         district_info = df[df['Provincia'] == district_name].to_dict(orient='records')
         print(district_info)
         return {"district_name": district_name, "district_info": district_info}
     else:
         return {"Error": "District not found"}
+    """
 
 
 @app.get('/module/search/{district_name}')
+
+
 def read_item_from_module(district_name: str):
     return {district_name}
 
 
 
 @app.get('/get-date')
+
+
 def get_date():
     """
     Endpoint to get the current date.
